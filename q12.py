3bbd9ac98c6e9b433d0a3b77ced9a8e04fddf81a
Hamza Hanif <hamzahanif2210@gmail.com>
2018-10-30 21:44:06 +0500

    Add files via upload

---
+++ q12.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 14:25:38 2018
+
+@author: Hp
+"""
+
+fruit = input('Enter the name of your favourite fruit.')
+
+
+while fruit != 'apple':
+    fruit = input('Enter the name of your favourite fruit.')
+
+        
